d50e643b1d0ca953ddaa1ddccf716607add8696e
Gael Gil <gaelgilbello27@gmail.com>
2020-12-22 16:01:17 -0800

    remade frontend server

--- routes/sneakers.js
+++ routes/sneakers.js
@@ -2,41 +2,45 @@
 
 const express = require('express');
 const router = express.Router();
-const models = require('../models');
+// const models = require('../models');
 
-
-// 
-// View all sneakers
-// 
-router.get('/', function(req, res, next) {
-    models.Sneakers.findAll().then(function(records){
-        res.render('sneakers/index', {
-        records: records
-        });
+    
+//Grabs all sneakers from the Sneakers database
+  router.get('/', function(req, res){
+    // models.Sneakers.findAll().then(function(sneakers){
+      res.render('sneakers/all', {
+      // sneakers: sneakers
       });
+    // });
   });
+
+
+  
+  
+//Grabs all sneakers from the Sneakers database
+router.get('/:id/', function(req, res){
+  // models.Sneakers.findByPk(req.params.id).then(function(snkr){
+  //   models.Sneakers.findAll().then(function(sneakers){
+      // console.log(snkr);
+      res.render('/sneakers/show', {
+      // sneakers: sneakers,
+      // snkr: snkr
+     });
+//   });
+// });
+});
   
 
-// 
-// View one sneaker
-// 
-router.get('/:id/', function(req, res, next) {
-    models.Sneakers.findByPk(req.params.id).then(function(snkr){
-        models.Sneakers.findAll().then(function(sneakers){
-          res.render('sneakers/sneaker', {
-          sneakers: sneakers,
-          snkr: snkr
-         });
-      });
-    });
-});
 
 
+    // router.get('/vis', function(req, res){
+    //     // models.Sneakers.findAll().then(function(sneakers){
+    //       res.render('/sneakers/visuals', {
+    //       // sneakers: sneakers
+    //       });
+    //     // });
+    // });
 
 
-
-
-
-
-
+      
 module.exports = router;
