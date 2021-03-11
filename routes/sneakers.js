1b30016edb9eb4eadcd78a3cfefa1302a185fe84
Gael Gil <gaelgilbello27@gmail.com>
2021-03-11 14:10:35 -0800

    added more api stuff and worked on frontend

--- routes/sneakers.js
+++ routes/sneakers.js
@@ -25,9 +25,10 @@ const sneaks = new SneaksAPI();
 router.get('/:id/detail', function(req, res){
   models.Sneakers.findByPk(req.params.id).then(function(snkr){
     models.Sneakers.findAll({ where: { sneakerName: snkr.sneakerName } }).then(function(sneakers){
-      // get data from api
+      // get data from the api using the styleID given
       sneaks.getProductPrices(snkr.styleID, function(err, product){
       res.render('sneakers/detail', {
+        // load data into html
       sneakers: JSON.stringify(sneakers),
       snkr: snkr,
       apiData: JSON.stringify(product),
@@ -42,26 +43,37 @@ router.get('/:id/detail', function(req, res){
 
 //Grabs all sneakers from the Sneakers database
 router.get('/vis', function(req, res){  
-sneaks.getProducts('nike', function(error, products){
-  models.Sneakers.findAll().then(function(sneakers){
-    res.render('sneakers/visuals', {
-    sneakers: sneakers,
-    maxSnkrs: JSON.stringify(products),
+  sneaks.getProducts('nike', function(error, products){
+    models.Sneakers.findAll().then(function(sneakers){
+      res.render('sneakers/visuals', {
+      sneakers: sneakers,
+      maxSnkrs: JSON.stringify(products),
+      });
     });
-  });
   })
 });
 
 
+// search using the api. then load data to html
+router.get('/search', function(req, res, next){  
+  if (req.query.search) {
+    sneaks.getProducts(req.query.search, function(error, products){
+      res.render('sneakers/search',{
+        products: products
+      })
+    })
+  }
+
+});
 
 
-    // router.get('/vis', function(req, res){
-    //     // models.Sneakers.findAll().then(function(sneakers){
-    //       res.render('/sneakers/visuals', {
-    //       // sneakers: sneakers
-    //       });
-    //     // });
-    // });
+router.get('/:id/more', function(req, res, next){
+  sneaks.getProductPrices(req.params.id, function(error, products){
+    res.render('sneakers/moreDetail', {
+      products:products,
+    })
+  })
+});
 
 
       
